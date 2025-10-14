871505606f47341c3882e0770239fdf0811c855a
Pratishtha R. <pratishtha.srao@gmail.com>
2025-10-14 18:15:41 -0500

    Revert "Added an on click UI to confirm on submit after filling the early access form"

--- components/WaitlistForm.tsx
+++ components/WaitlistForm.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import AfterSubmit from "./AfterSubmit";
 
 type FormData = {
   name: string;
@@ -52,7 +51,7 @@ export default function WaitlistForm() {
       className="bg-[#0D0D0D] text-white p-6 rounded-2xl max-w-xl mx-auto space-y-4 shadow-lg"
     >
       {submitted ? (
-        <AfterSubmit></AfterSubmit>
+        <p className="text-center text-lg">You're in. Expect updates soon.</p>
       ) : (
         <>
           <input
