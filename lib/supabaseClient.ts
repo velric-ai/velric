4cc8ef4784f5854a8e5ed2fb64165fb0ae624b57
Pratishtha Rao <pratishtha.srao@gmail.com>
2025-11-05 18:12:01 -0600

    Fixing deployment issues

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -46,7 +46,6 @@ import { createClient } from "@supabase/supabase-js";
 /*
 const supabaseUrl =
   process.env.NEXT_PUBLIC_SUPABASE_URL ||
-  "https://yzszgcnuxpkvxueivbyx.supabase.co";
 const supabaseKey =
   process.env.SUPABASE_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 */
