ae37d5aba4bcee30de587e2d6e2bac5c47225f2f
arvindkhandal <arvindkhandal516@gmail.com>
2025-12-10 13:24:09 +0530

    remove env var from console

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -63,7 +63,6 @@ if (!supabaseUrl || !supabaseAnonKey) {
   if (isBrowser) {
     console.error(
       "⚠️ Supabase environment variables are missing. Check NEXT_PUBLIC_SUPABASE_URL and NEXT_PUBLIC_SUPABASE_ANON_KEY."
-      ,supabaseUrl, supabaseAnonKey, process.env.SUPABASE_SERVICE_ROLE_KEY
     );
   } else {
     console.warn(
